a354da68c4a65a550b2c35fe3c190b97b622e4f2
Firefly-Sandbox <160746248+Firefly-Sandbox@users.noreply.github.com>
2024-02-22 11:20:42 -0500

    Generate TF Module: provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+# The code was generated for this provider version (it can be changed to your preference). 
+
+# The region is based on the first chosen resource (detected multiple regions). 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.8.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
