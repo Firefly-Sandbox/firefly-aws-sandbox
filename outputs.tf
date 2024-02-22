ce8861d228573c81e13a413c87cde1e0bfbb0a83
Firefly-Sandbox <160746248+Firefly-Sandbox@users.noreply.github.com>
2024-02-22 11:20:42 -0500

    Generate TF Module: outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,32 @@
+output "aws_iam_role_GItHubActions-782_id" {
+  value = "${aws_iam_role.GItHubActions-782.id}"
+}
+
+output "aws_vpc_Fargate-Internal-217_id" {
+  value = "${aws_vpc.Fargate-Internal-217.id}"
+}
+
+output "aws_default_network_acl_acl-07198e3a534db93dc-8de_id" {
+  value = "${aws_default_network_acl.acl-07198e3a534db93dc-8de.id}"
+}
+
+output "aws_route_table_rtb-064898d79749b3008-b34_id" {
+  value = "${aws_route_table.rtb-064898d79749b3008-b34.id}"
+}
+
+output "aws_iam_role_AmazonEKSFargatePodExecutionRole-e00_id" {
+  value = "${aws_iam_role.AmazonEKSFargatePodExecutionRole-e00.id}"
+}
+
+output "aws_vpc_firefly-sandbox-vpc-02f_id" {
+  value = "${aws_vpc.firefly-sandbox-vpc-02f.id}"
+}
+
+output "aws_route_table_rtb-0dc6d472eaf87747b-dec_id" {
+  value = "${aws_route_table.rtb-0dc6d472eaf87747b-dec.id}"
+}
+
+output "aws_default_network_acl_acl-0ea4c444fb06f0516-86d_id" {
+  value = "${aws_default_network_acl.acl-0ea4c444fb06f0516-86d.id}"
+}
+
